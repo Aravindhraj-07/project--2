e43d5e17362e918416869460709d9aca957f1bfa
Aravindhraj-07 <aravindraj98106@gmail.com>
2026-03-28 22:09:49 +0530

    Added new changes

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import joblib
+
+app = Flask(__name__)
+
+# Load model
+pipeline = joblib.load("fake_news_detector.pkl")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    prediction = None
+
+    if request.method == "POST":
+        news = request.form["news"]
+
+        if news.strip() != "":
+            result = pipeline.predict([news])[0]
+
+            if result == 0:
+                prediction = "Fake News ❌"
+            else:
+                prediction = "Real News ✅"
+
+    return render_template("index.html", prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
