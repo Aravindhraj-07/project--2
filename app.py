0b9a01cdfb4e7b3c1d4cb71444beabee59486a38
Aravindhraj-07 <aravindraj98106@gmail.com>
2026-03-28 23:25:35 +0530

    Added new changes

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+import re
+
 from flask import Flask, render_template, request
 import joblib
 
 app = Flask(__name__)
+import re
+def clean_text(text):
+    text = str(text).lower()
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"[^a-zA-Z]", " ", text)
+    text = re.sub(r"\s+", " ", text).strip()
+    return text
 
 # Load model
 pipeline = joblib.load("fake_news_detector.pkl")
@@ -10,11 +19,19 @@ pipeline = joblib.load("fake_news_detector.pkl")
 def home():
     prediction = None
 
+
+    if request.method == "POST":
+        news = request.form["news"]
+        
+
+ 
+
     if request.method == "POST":
         news = request.form["news"]
 
         if news.strip() != "":
-            result = pipeline.predict([news])[0]
+            cleaned = clean_text(news)
+            result = pipeline.predict([cleaned])[0]
 
             if result == 0:
                 prediction = "Fake News ❌"
@@ -24,4 +41,4 @@ def home():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000, debug=False)
